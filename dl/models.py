9a9cc650512db4ed11b0b672582380ad6793cb94
ikuma <chemicalbondo@gmail.com>
2023-02-07 11:49:21 +0900

    メディアファイルの格納場所の設定変更

--- dl/models.py
+++ dl/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class UploadFile(models.Model):
-    file = models.FileField(upload_to='dl/upload_files/')
+    file = models.FileField()
     file_name = models.CharField(max_length=100, verbose_name='ダウンロード時に使うファイル名')
     serial_number = models.CharField(max_length=100, verbose_name='製造番号', blank=False, null=False, unique=True,
-                                     default='a')
+                                     default='')
 
     def __str__(self):
         return self.file_name
