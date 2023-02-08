9cb8446b6755a4f84ff583c2c095d61aeb811973
ikuma <chemicalbondo@gmail.com>
2023-02-08 17:12:32 +0900

    UploadFileモデルのfileフィールドをnull=True, blank=Trueにした

--- dl/models.py
+++ dl/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class UploadFile(models.Model):
-    file = models.FileField()
+    file = models.FileField(null=True, blank=True)
     file_name = models.CharField(max_length=100, verbose_name='ダウンロード時に使うファイル名')
     serial_number = models.CharField(max_length=100, verbose_name='製造番号', blank=False, null=False, unique=True,
                                      default='')
