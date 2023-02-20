e4beb5dd605a8a67811b001fbc5a7947ed511018
ikuma <chemicalbondo@gmail.com>
2023-02-20 16:03:01 +0900

    tiff画像をjpegに変換して返すビューを追加

--- dl/models.py
+++ dl/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class UploadFile(models.Model):
-    file = models.FileField(null=True, blank=True, verbose_name='ダウンロード時に使うファイル名')
+    file = models.FileField(null=True, blank=True, verbose_name='ダウンロード時に使うファイル名', upload_to='images')
     file_name = models.CharField(max_length=100, verbose_name='ダウンロード時に使うファイル名')
     serial_number = models.CharField(max_length=100, verbose_name='製造番号', blank=False, null=False, unique=True,
                                      default='')
